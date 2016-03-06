abcebea4739cd3fb4256451433eda7b8c025ad03
Brenden Kokoszka <brenden.kokoszka@gmail.com>
2016-03-06 14:55:16 -0800

    Styling input string textbox

--- UkkonenVisualization.elm
+++ UkkonenVisualization.elm
@@ -2,6 +2,8 @@ module UkkonenVisualization (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Text
+import Color exposing (..)
 import Graphics.Element exposing (..)
 import Graphics.Input.Field exposing (..)
 import Json.Encode as Json
@@ -10,7 +12,9 @@ import UkkonenTree exposing (..)
 import UkkonenAlgorithm exposing (..)
 
 
-defaultInput = "abcabxabzxz"
+defaultInput =
+    "abcabxabzxz"
+
 
 port tree : Signal Json.Value
 port tree =
@@ -22,8 +26,21 @@ inputString =
     Signal.mailbox noContent
 
 
+inputFieldStyle : Style
+inputFieldStyle =
+    let
+        textDefaultStyle = Text.defaultStyle
+    in
+        { defaultStyle
+            | padding = uniformly 1
+            , outline = { color = rgb 255 0 0, width = uniformly 2, radius = 4 }
+            , style = { textDefaultStyle | height = Just 15, color = rgb 123 53 20 }
+        }
+
+
 inputField : Content -> Element
-inputField = field defaultStyle (Signal.message inputString.address) defaultInput
+inputField =
+    field inputFieldStyle (Signal.message inputString.address) defaultInput
 
 
 main : Signal Html
@@ -32,8 +49,9 @@ main =
 
 
 view : Content -> Html
-view content = section [ id "visualization" ]
-    [
-      h1 [] [text "Visualization of Ukkonen's Algorithm"],
-      inputField content |> fromElement
-    ]
+view content =
+    section
+        [ id "visualization" ]
+        [ h1 [] [ text "Visualization of Ukkonen's Algorithm" ]
+        , inputField content |> fromElement
+        ]
