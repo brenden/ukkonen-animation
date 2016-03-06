9cc42b9a02c534acb16d6dc6ed68150c23366594
Brenden Kokoszka <brenden.kokoszka@gmail.com>
2016-03-06 14:24:50 -0800

    Starting on UI layout

--- UkkonenVisualization.elm
+++ UkkonenVisualization.elm
@@ -1,7 +1,7 @@
 module UkkonenVisualization (..) where
 
-import Html exposing (pre, text)
-import Html.Attributes exposing (class)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Graphics.Element exposing (..)
 import Graphics.Input.Field exposing (..)
 import Json.Encode as Json
@@ -9,6 +9,8 @@ import Window
 import UkkonenTree exposing (..)
 import UkkonenAlgorithm exposing (..)
 
+
+defaultInput = "abcabxabzxz"
 
 port tree : Signal Json.Value
 port tree =
@@ -20,6 +22,18 @@ inputString =
     Signal.mailbox noContent
 
 
-main : Signal Element
+inputField : Content -> Element
+inputField = field defaultStyle (Signal.message inputString.address) defaultInput
+
+
+main : Signal Html
 main =
-    Signal.map (field defaultStyle (Signal.message inputString.address) "Input string") inputString.signal
+    Signal.map view inputString.signal
+
+
+view : Content -> Html
+view content = section [ id "visualization" ]
+    [
+      h1 [] [text "Visualization of Ukkonen's Algorithm"],
+      inputField content |> fromElement
+    ]
